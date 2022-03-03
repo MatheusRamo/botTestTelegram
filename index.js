dc8a6c7ddb3a4491f9dc9505823bf3405aa8344e
Matheus <geof.matheus@gmail.com>
2022-03-03 02:40:31 -0300

    add help

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { Composer } = require('micro-bot')
 
 const bot = new Composer()
 bot.start((ctx) => ctx.reply('Welcome')) // display Welcome text when we start bot
+bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there')) // listen and handle when user type hi text
 
 module.exports = bot
