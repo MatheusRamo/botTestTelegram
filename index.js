e9f86833514457c70f5611c93fce6acb8d29c391
Matheus <geof.matheus@gmail.com>
2022-03-03 07:43:01 -0300

    u

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const { Composer } = require('micro-bot')
-const convertapi = require('convertapi')(process.env.API_SECRET)
+// const convertapi = require('convertapi')(process.env.API_SECRET)
+const download = require('download')
+
+const libre = require('libreoffice-convert')
+const fs = require('fs')
+
+function convertToPdf(filePath) {
+
+   const file = fs.readFileSync(filePath)
+ 
+   libre.convert(file, extend, undefined, (err, done) => {
+      if (err) {
+        console.log(`Error converting file: ${err}`)
+      }
+ 
+      fs.writeFileSync('outputFile.pdf', done)
+  })
+}
 
 
 const bot = new Composer()
@@ -10,11 +27,21 @@ bot.on('document', async (ctx) => {
     const { file_id: fileId } = ctx.update.message.document
     const fileUrl = await ctx.telegram.getFileLink(fileId)
 
-    const resultPromise = await convertapi.convert('pdf', { File: fileUrl })
-    const pdfUrl = await resultPromise.file.url
 
-    ctx.reply(`Url: ${fileUrl}\n\n Pdf Url: ${pdfUrl}`)
-    ctx.replyWithDocument(pdfUrl)
+    // Download the file
+    (async () => {
+        await download(fileUrl, './')
+    })()
+
+
+
+    // const resultPromise = await convertapi.convert('pdf', { File: fileUrl })
+    // const pdfUrl = await resultPromise.file.url
+
+    // ctx.reply(`Url: ${fileUrl}\n\n Pdf Url: ${pdfUrl}`)
+    // ctx.replyWithDocument(pdfUrl)
+
+    ctx.reply(`FileId: ${fileId}`)
 })
 
 module.exports = bot
