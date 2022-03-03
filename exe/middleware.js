99badaf309e40d2c5d02ccc93f10632b6a66eb5e
Matheus <geof.matheus@gmail.com>
2022-03-02 22:52:13 -0300

    add .env.example

--- exe/middleware.js
+++ exe/middleware.js
@@ -5,3 +5,20 @@ const exec = (ctx, ...middleware) => {
     run(0)
 }
 
+const midd1 = (ctx, next) => {
+    ctx.info1 = "info1"
+    next()
+}
+
+const midd2 = (ctx, next)=>{
+    ctx.info2 = "info2"
+    next()
+}
+
+const midd3 = ctx => ctx.info3 = "info3"
+
+
+const ctx = {}
+exec(ctx, midd1, midd2, midd3)
+
+console.log(ctx)
