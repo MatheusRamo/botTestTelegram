fb22dbd1e869f43d0289f4a8d19f052c183a575c
Matheus <geof.matheus@gmail.com>
2022-03-03 02:04:55 -0300

    add Procfile

--- firstBot.js
+++
@@ -1,6 +0,0 @@
-const Telegraf = require('telegraf') // import telegram lib
-
-const bot = new Telegraf('5125681835:AAH43gW3B4fSyRj1IaHjCkdmx4PwCid6Zj8') // get the token from envirenment variable
-bot.start((ctx) => ctx.reply('Welcome')) // display Welcome text when we start bot
-bot.hears('hi', (ctx) => ctx.reply('Hey there')) // listen and handle when user type hi text
-bot.launch() //start
